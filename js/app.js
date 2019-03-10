90ae3dea0c0f1661969551c52119889ca3d5dcdf
magdastrojwas <magdalena.strojwas@gmail.com>
2019-03-10 18:44:01 +0100

    new texts = ga+ gallery

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
-$(function() {
+// $(function() {
 
-        $('.window_content a.window_close').on('click', function(e) {
-            e.preventDefault();
+//         $('.window_content a.window_close').on('click', function(e) {
+//             e.preventDefault();
 
-            $(this).parent().removeClass('opened');
-            $('#window_overlay').removeClass('opened');
-        });
+//             $(this).parent().removeClass('opened');
+//             $('#window_overlay').removeClass('opened');
+//         });
 
-        $('.window_content').addClass('opened');
-        $('#window_overlay').addClass('opened');
+//         $('.window_content').addClass('opened');
+//         $('#window_overlay').addClass('opened');
+//     });
+// MDB Lightbox Init
+
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
     });
-
